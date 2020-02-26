8c966cc9fdaf1cb4802a2dc2bb75e879eac61dfd
Robert <roobtran@gmail.com>
2020-02-25 21:39:42 -0500

    +Set up multiplayer

---
+++ Defence-Game/Assets/NetworkController.cs
@@ -0,0 +1,36 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connected" + PhotonNetwork.CloudRegion);
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Roon", new RoomOptions { MaxPlayers = 2 }, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player", new Vector3(Random.Range(-0.9f,-1f),transform.position.y,transform.position.z), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
