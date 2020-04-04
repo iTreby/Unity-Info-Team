bc808f93a1315f496ba8f01274acf268d6443f92
Robert <roobtran@gmail.com>
2020-04-04 01:51:39 -0400

    +Update main character

--- Defence-Game/Assets/Scripts/NetworkController.cs
+++ Defence-Game/Assets/Scripts/NetworkController.cs
@@ -6,6 +6,22 @@ using Photon.Realtime;
 
 public class NetworkController : MonoBehaviourPunCallbacks
 {
+
+    private static NetworkController _instance = null;
+
+    public static NetworkController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<NetworkController>();
+            }
+            return _instance;
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +42,7 @@ public class NetworkController : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         //
-        PhotonNetwork.Instantiate("Viking", new Vector3(Random.Range(1f,1.5f),0.32f,Random.Range(35f,55f)), Quaternion.identity);
+        PhotonNetwork.Instantiate("Hero", new Vector3(Random.Range(1f,1.5f),0.32f,Random.Range(35f,55f)), Quaternion.identity);
     }
 
     // Update is called once per frame
