df34554a0854ff688259a27c61dbb6ec43af8909
Robert <roobtran@gmail.com>
2020-04-06 00:16:35 -0400

    +Update game

---
+++ Defence-Game/Assets/Scripts/ChairHP.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChairHP : MonoBehaviour
+{
+
+    [SerializeField] float maxHealth = 100.0f;
+    [SerializeField] Health health;
+    private float currentHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TakeDamage(float damage)
+    {
+        currentHP -= damage;
+        health.SetHealth(currentHP);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "soul")
+        {
+            TakeDamage(5);
+        }
+    }
+
+
+
+     
+
+}
