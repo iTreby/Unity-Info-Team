04fe7794186b62c1cebfc404d1ba946595416e37
Robert <roobtran@gmail.com>
2020-04-15 02:33:58 -0400

    +Added winning condition

---
+++ Defence-Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] Text timer;
+    [SerializeField] GameObject winningState;
+    private float timeLeft = 120.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winningState.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        timer.text = "TIME : " + (int)timeLeft;
+        if(timeLeft < 0)
+        {
+            timer.text = "You Win";
+            winningState.SetActive(true);
+        }
+    }
+}
--- Defence-Game/Assets/Tests/EditMode/NewTestScript.cs
+++ Defence-Game/Assets/Tests/EditMode/NewTestScript.cs
@@ -8,6 +8,7 @@ namespace Tests
 {
     public class NewTestScript
     {
+    
       
     }
 }
